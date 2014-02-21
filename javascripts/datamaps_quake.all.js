34a10aac14bacd0341908b53d32f1a54c9bf997b
sergethompson <serge.thompson@gmail.com>
2014-02-21 14:46:03 -0500

    at super awesome state

--- javascripts/datamaps_quake.all.js
+++ javascripts/datamaps_quake.all.js
@@ -417,7 +417,7 @@
           return i * 100; 
         })                                                              //changes time to put bubbles on screen
           .attr('r', function ( datum ) {
-            return .5*Math.exp(datum.magnitude)/Math.exp(datum.magnitude*.6);
+            return .45*Math.exp(datum.magnitude)/Math.exp(datum.magnitude*.45);
           });
 
     bubbles.exit()
